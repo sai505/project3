38ad8039202bd1437b669519ae0ca384cd4f589b
sai505 <seungjun88@gmail.com>
2017-12-10 23:06:20 -0500

    Create test_dijkstra.cpp

---
+++ test_dijkstra.cpp
@@ -0,0 +1,55 @@
+// test_dijkstra.cpp
+// c. 2017 T. O'Neil, C. Reilly
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Digraph.hpp"
+
+using std::ifstream;
+
+int main() {
+	Digraph g;
+	ifstream dataFile;
+	int numPoints, p, q, r;
+	string city;
+
+	dataFile.open("nqmq.dat");
+	dataFile >> numPoints;
+	std::cout<<numPoints;
+	std::cout << "menu:" << std::endl;
+	for (int i = 0; i < numPoints; i++) {
+		dataFile >> city;
+		g.addVertex(city);
+		std::cout << i << " : " << city << std::endl; //menu
+	} 
+	g.resetEdges();
+	dataFile >> p; // source
+	dataFile >> q; // destination
+	dataFile >> r; // weight
+
+	while (p > -1) {
+		g.addEdge(p, q, r);
+		g.addEdge(q, p, r);
+		dataFile >> p;
+		dataFile >> q;
+		dataFile >> r;
+	}
+	dataFile.close();
+
+
+	cout << "TEST 1. Los Angeles to Boston" << endl;
+	p = g.dijkstra(4, 1);
+	cout << "*** Final distance: " << p << " miles." << endl;
+	if (p != 2602) cout << "TEST FAILED";
+	else cout << "Test passed";
+	cout << endl << endl;
+
+	cout << "TEST 2. San Francisco to Miami" << endl;
+	p = g.dijkstra(7, 5);
+	cout << "*** Final distance: " << p << " miles." << endl;
+	if (p != 3056) cout << "TEST FAILED";
+	else cout << "Test passed";
+	cout << endl << endl;
+
+}
